edfc0e2f8484dd79a31cd4df61ce9acf95d6003d
dillondesilva <dillon.desilva@geeksdiary.com>
2024-04-30 10:17:27 +1000

    new example project tidied

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,10 @@
-import './App.css';
-import PythonEditor from 'codehelium';
-import { useEffect, useState } from 'react';
+import PythonEditor from 'codehelium'
 
 function App() {
-  const [consoleOutputs, setConsoleOutputs] = useState([]);
-
-  useEffect(() => {
-    console.log("Output from my IDE react component is", consoleOutputs);
-  }, [consoleOutputs, setConsoleOutputs])
-
   return (
-    <div>
-      <PythonEditor width="90vw" height="80vh" consoleOutputSetter={setConsoleOutputs}/>
+    <div style={{top: "50%", left: "50%", position: "absolute", 
+    transform: "translate(-50%, -50%)"}}>
+      <PythonEditor height="80vh" width="90vw" />
     </div>
   );
 }
