c9c5cc16597f43826cef62eaa5da1b9cc1baaa5d
dillondesilva <dillon.desilva@geeksdiary.com>
2024-04-30 19:19:43 +1000

    updated example

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{top: "50%", left: "50%", position: "absolute", 
     transform: "translate(-50%, -50%)"}}>
-      <PythonEditor height="80vh" width="90vw" />
+      <PythonEditor height="80vh" width="90vw" enableDownload={true} />
     </div>
   );
 }
